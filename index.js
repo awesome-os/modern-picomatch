bd3512150844941681d1fe499a322822587aea2b
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2025-08-09 09:59:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ import { picomatch as pico, picomatchNode } './lib/picomatch.js');
 export * from './lib/constants.js';
 export * as utils from './lib/utils.js';
 export const picomatch = (glob, options, returnState = false) => pico(glob, picomatchNode(options), returnState);
+export default picomatch;
